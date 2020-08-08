a51bb418f0e5b9611aa7e76216a01fd99ba67a0f
q90016200 <q90016200@gmail.com>
2020-08-08 15:53:05 +0800

    update auth

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use App\Http\Controllers\Controller;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Log;
+
+class AuthController extends Controller
+{
+    public function user(){
+        return Auth::user();
+    }
+
+    public function login(Request $request) {
+        $user = User::where('email', $request->email)->first();
+
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            throw new AuthenticationException();
+        }
+
+        return response([
+            'access_token' => $user->createToken("token-name")->plainTextToken,
+        ]);
+    }
+
+    public function logout() {
+        // if (Auth::check()) {
+            Auth::user()->tokens()->delete();
+
+            return response([
+                "code" => 0,
+                "message" => "logout success"
+            ], 204);
+        // } else {
+    //     return response([
+        //         "code" => 400,
+        //         "message" => "user not logined"
+        //     ], 400);
+        // }
+    }
+}
